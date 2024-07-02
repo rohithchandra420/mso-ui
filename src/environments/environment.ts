ba8fe4a5ecacad7bca6edae06acb4f4d165efcd3
rohithchandra420 <rohithchandra@gmail.com>
2024-07-02 14:40:27 +0530

    Updated shop product card background and envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    URL:'https://mso-api.onrender.com',
-    RAZ_API_KEY: 'rzp_test_Y9m41KlJg2w3JL'
+    URL: 'https://makesomeochha-api.onrender.com',
+    RAZ_API_KEY: 'rzp_test_wcR526vtI1dqMP'
 };
